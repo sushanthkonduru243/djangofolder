4c2974a073015c8b0fbf4d43f8ee1e0d0697f7b0
sushanthkonduru243 <2410080078@klh.edu.in>
2025-09-30 23:40:11 +0530

    Initial Commit

---
+++ calc/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html', {'name': 'Raju'})
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    val3 = val1 + val2
+    return render(request, 'result.html', {'result': val3})
+
